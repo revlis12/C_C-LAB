36494e1a912d75a766c61b6d9753d8cff32fd812
revlis12 <silver20079@gmail.com>
2026-09-15 12:35:58 +0900

    Add files via upload

---
+++ 3주차/LAB_5-4.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main() 
+{
+    int a, b;
+    int temp;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    temp = b;
+    while(temp > 0) 
+    {
+        printf("%d\n", a*(temp % 10));
+        temp /= 10;
+    }
+
+    printf("%d", a*b);
+    return 0;
+}
